a28c3a8d6f92b7e6ea4663339d5ea5da48a5222f
Adrita Chatterjee <chatterjiadrita@gmail.com>
2024-08-01 19:35:06 +0530

    cast array

--- backend/api1/models/movie.js
+++ backend/api1/models/movie.js
@@ -6,7 +6,7 @@ const movieSchema = new mongoose.Schema({
     duration: {type:Number,required:true},
     genre: {type:String,required:true},
     rel_date: {type:String,required:true},
-    cast: {type:String,required:true},
+    cast: {type:[String],required:true},
     director: {type:String,required:true},
     imdb_rating: {type:Number,required:true},
     poster:{type:String,required:true},
