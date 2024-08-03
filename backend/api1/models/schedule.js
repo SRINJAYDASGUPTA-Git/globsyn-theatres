f11a76a78940e5cd6dbd4357384d88a0e047aec8
Adrita Chatterjee <chatterjiadrita@gmail.com>
2024-08-03 16:24:41 +0530

    Get mappings done for movie, screen, schedule

--- backend/api1/models/schedule.js
+++ backend/api1/models/schedule.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const scheduleSchema = new mongoose.Schema({
   date: { type: Date, required: true },
-  start_time: { type: String, required: true }, // Store time as a string in HH:mm format
-  end_time: { type: String, required: true }, // Store time as a string in HH:mm format
+  start_time: { type: String, required: true }, 
+  end_time: { type: String, required: true }, 
   movie: { type: mongoose.Schema.Types.ObjectId, ref: "Movie", required: true },
-  screen: { type: Number, required: true },
+  screen: { type: mongoose.Schema.Types.ObjectId, ref: "Screen", required: true },
 });
 
 const Schedule = mongoose.model("Schedule", scheduleSchema);
