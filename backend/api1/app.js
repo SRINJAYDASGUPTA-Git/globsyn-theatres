383ac78aab795955825e34defa7399c1b52f1a05
Srinjay Das Gupta <70833470+SRINJAYDASGUPTA-Git@users.noreply.github.com>
2024-08-07 05:14:55 +0000

    chore(api): Update MongoDB connection string with environment variables

--- backend/api1/app.js
+++ backend/api1/app.js
@@ -3,10 +3,13 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const swaggerUi = require("swagger-ui-express");
 const swaggerJsDoc = require("swagger-jsdoc");
-
+require('dotenv').config()
 const mongoose = require("mongoose");
+const USERNAME = process.env.MONGO_INITDB_ROOT_USERNAME;
+const PASSWORD = process.env.MONGO_INITDB_ROOT_PASSWORD;
+
 mongoose
-  .connect("mongodb://localhost:27017")
+  .connect(`mongodb://${USERNAME}:${PASSWORD}@localhost:27017`)
   .then(() => {
     console.log("Connection establised!");
   })
