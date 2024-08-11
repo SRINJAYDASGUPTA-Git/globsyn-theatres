8b10b2e18437b075929aa438046d6d94a9c8271f
SRINJAYDASGUPTA-Git <dasguptasrinjay2004@gmail.com>
2024-08-11 23:01:53 +0530

    chore: Update movie component to fetch and display movies on home page

--- frontend/cinebook/src/app/pages/home/home.component.ts
+++ frontend/cinebook/src/app/pages/home/home.component.ts
@@ -1,35 +1,34 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Movie } from '../../services/models';
 import { movieGet } from '../../services/fn/movies/movie-get';
+import { MoviesService } from '../../services/services';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-
 export class HomeComponent {
-
-  movies:Movie[]=[];
+  movies: Movie[] = [];
 
   constructor(
     private router: Router,
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private movieService: MoviesService
+  ) {}
 
-  ngOnInit():void{
+  ngOnInit(): void {
     this.getrecentmovies();
   }
-  getrecentmovies():void{
-    const rootUrl = 'http://localhost:5000';
-  
-  movieGet(this.http, rootUrl).subscribe((response) => {
-    this.movies = response.body || []; 
-  })};
+  getrecentmovies(): void {
+    this.movieService.movieGet().subscribe((movies) => {
+      this.movies = movies;
+    });
+  }
 
-  login(){
+  login() {
     this.router.navigate(['/login']);
   }
 }
